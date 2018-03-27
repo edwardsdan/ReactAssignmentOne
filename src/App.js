7de38f303628df30c11ad05df7fa2f3b59dae94b
DanE <edwardsdan001@gmail.com>
2018-03-27 18:55:24 -0400

    Finished js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import UserInput from './User/UserInput';
+import UserOutput from './User/UserOutput';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    
+    userName: 'sampleName'
   };
+
+  switchHandler = (newUserName) => {
+    this.setState({
+      userName: newUserName
+    });
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState({
+      userName: event.target.value
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        <UserInput />
-        <UserOutput userName="sampleName"/>
-        <UserOutput />
+        <button onClick={() => this.switchHandler('newUserName')}>Switch Name</button>
+        <UserInput changed={this.inputChangedHandler.bind(this)} name={this.state.userName}/>
+        <UserOutput userName={this.state.userName}/>
+        <UserOutput userName={this.state.userName}/>
       </div>
     );
   }
